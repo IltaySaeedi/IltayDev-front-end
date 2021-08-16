10bf1b7a89e689cac1cd6ffe54d80f6c8e6f88de
Iltay Saeedi <iltay_saeedi@hotmail.com>
2021-08-16 12:34:06 +0430

    Changing content

--- scripts/script-ie.js
+++ scripts/script-ie.js
@@ -50,8 +50,11 @@ function hardCoding(langName) {
         langLabel.innerText = "تغییر زبان";
         langSubmit.innerText = "انتخاب زبان";
         main.firstElementChild.innerText = "خوش آمدید";
-        main.children[1].innerText = "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که ";
-        main.children[2].innerText = "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای ";
+        main.children[1].innerText = "این صفحه وب مرجع من برای کاوش کتابخانه ها، فریم ورک ها وب دیگر است.";
+        main.children[2].innerText = "انگیزه";
+        main.children[3].innerText = "قصد داشتم با توجه مواردی که از سایت هایی مانند MDN، W3Schools و CSS Tricks یاد گرفتم یک صفحه ی وب بسازم. می توانستم ویژگی های دیگری هم اضافه کنم اما این هدفم نبود شاید بعدا برای بهبود این صفحه تلاش کنم. من بهترین UI رو طراحی نکردم، من آنچه که دوست داشتم رو طراحی کردم.";
+        main.children[4].innerText = "هدف";
+        main.children[5].innerText = " هنگام استفاده از فریم ورک های وب مشکلاتی داشتم زیرا دقیقا نمیدانستم باید چه کاری انجام دهم و تقریبا در ویژگی های مختلف فریم ورک ها گم بودم. بنابراین من یک قدم رو به عقب برداشتم و به موارد ساده تر پرداختم. اکنون احساس می کنم می توانم خودم را برای انجام کاری مشابه در فریم ورک های دیگر به چالش بکشم. اما فکر می کنم به آسانی که به نظر می رسد نخواهد بود زیرا من به ویژگی های جدیدی که در کد می بینم شک میکنم.";
     } else {
         navItems[0].innerText = "Home";
         navItems[1].innerText = "Posts";
@@ -63,8 +66,11 @@ function hardCoding(langName) {
         langLabel.innerText = "Change your language";
         langSubmit.innerText = "Change language";
         main.firstElementChild.innerText = "Welcome";
-        main.children[1].innerText = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus cupiditate fugit veritatis iusto, nostrum obcaecati vero autem harum optio veniam dolorem. Nemo error doloribus possimus deserunt non dolores, iste aspernatur!";
-        main.children[2].innerText = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae corporis veniam soluta, reiciendis, enim nisi ratione impedit tempora repellat architecto nostrum nihil reprehenderit aperiam quis tenetur quaerat sed dolore magni deleniti sint animi! Est deserunt magni sit fuga, reprehenderit consequatur qui quis iste? Possimus saepe doloremque beatae consequatur officia voluptatem?";
+        main.children[1].innerText = "This is my reference web page for discovering other web libraries, framewokrs or templates.";
+        main.children[2].innerText = "Motivation";
+        main.children[3].innerText = "I wanted to create a single web page according to what I learned from MDN, W3Schools, CSS Tricks or other resources. I needed to do it simple, I could add more features but it wasn't my goal maybe I'll do it later. I didn't write best UI, I wrote what I liked.";
+        main.children[4].innerText = "Purpose";
+        main.children[5].innerText = "I had an issue when I was using web frameworks because I didn't know exactly what to do and I was almost lost in the features that I didn't fully understand. So I took a step back and started discovering simpler things. Now I feel I can challenge myself for doing the same in other web libraries, framewokrs or templates, but I think it is not as easy as it seems because I have doubt on new features that I see.";
     }
 }
 
